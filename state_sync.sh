3127d2d1e115a0650cf97a6faa6fed956a7b4169
hoanguyenkh <hoank@astranaut.io>
2023-10-12 15:31:20 +0700

    refactor: update seed note

--- state_sync.sh
+++ state_sync.sh
@@ -26,7 +26,7 @@ s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
 s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
 s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"\"|" $HOME/.astrad/config/config.toml
 
-sed -i.bak -E 's#^(seeds[[:space:]]+=[[:space:]]+).*$#\1"665532046ce09bb9c364826cc6b589ca89eddea2@34.87.148.96:26656,2c7f17ac837a023270f137d4c8e66a6757c7f0be@34.96.182.32:26656"#' ~/.astrad/config/config.toml
+sed -i.bak -E 's#^(seeds[[:space:]]+=[[:space:]]+).*$#\1"665532046ce09bb9c364826cc6b589ca89eddea2@34.87.148.96:26656,a99a441714f2d11c69b5746e4ff6ad64a9fa0ad7@34.92.235.232:26656"#' ~/.astrad/config/config.toml
 sed -i.bak -E 's#^(timeout_commit[[:space:]]+=[[:space:]]+).*$#\1"2400ms"#' ~/.astrad/config/config.toml
 
 # astrad start --x-crisis-skip-assert-invariants
